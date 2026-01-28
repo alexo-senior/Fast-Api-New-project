441958df93a16e276df9a67cb050d560cc5e018c
alexis <alexobarrafer@gmail.com>
2026-01-27 21:37:27 -0500

    se crea una Clase modelo,y el endpoint post, se aplica db.add(), ded.commit(), y db.close()

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,9 @@
 # este archivo es la raiz y va toda la aplicacion 
 from typing import Annotated
+
+from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status
 # imprrtar los modelos para crear las tablas en la bd
 import models
 from models import Todos
@@ -22,8 +24,55 @@ def get_db():
         db.close() # se ejecuta el cierre de la bd despues de enviar la respuesta
 # asi se asegura que solo se abra una conexion a la bd por cada peticion que se haga a la app
 
+# simplifica la declaracion de dependencias 
+
+db_dependency = Annotated[Session, Depends(get_db)]
         
-@app.get("/")     
+"""@app.get("/")     
 async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()"""
+
+#creamos una clase basada en BaseModel de pydantic
+
+class TodoRequest(BaseModel):
+    # el id no se pasa por ser clave primaria y debe ser autoincremental
+    title:str = Field(min_length=3)
+    description:str = Field(min_length=3, max_length=100)
+    priority:int = Field(gt=0, lt=6)
+    complete:bool = Field(default=False)
+
+
+
+@app.get("/")     
+async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+# AÑADIR FUNCIONALIDADES 
+# con parametro de ruta 
+#se agrega status code 200 ok para validacion de exito
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+
+# se agrega path para validar que el id sea mayor a 0
+async def read_todo(db:db_dependency, todo_id:int = Path(gt=0)):
+
+    # la bd se obtiene del modelo, filtros id, y se obtiene el primer resultado
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    #si no es none que devuelva el modelo
+    if todo_model is not None:
+        return todo_model
+    # sino lanzamos una excepcion
+    raise HTTPException(status_code=404, detail='Todo not found')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db:db_dependency, todo_request:TodoRequest):
+    todo_model = Todos(**todo_request.dict())
+
+    db.add(todo_model)
+    db.commit() # guarda los cambios en la bd
+    db.refresh(todo_model)# recarga el modelo con los datos de la bd
+    return todo_model# devuelve el modelo creado 
+
 
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -7,13 +7,17 @@ from sqlalchemy import Column, Integer , String, Boolean
 
 class Todos(Base):
     __tablename__='todos' #nombre de la tabla
-    # id va a ser una columna de tipo entero en nuestra bd 
+
+    # id va a ser una columna de tipo entero en nuestra bd
+
     # primar key porque sera un registro unico index para darle facilicidad de busqueda
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+
     
     
     
